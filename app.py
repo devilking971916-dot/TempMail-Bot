fcb06585ef7a8af3613768c73ea6c8e0185ff976
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 03:49:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import threading
+from tempmail import bot  # tumhara pyrogram client
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running ✅"
+
+def run_bot():
+    bot.run()
+
+threading.Thread(target=run_bot).start()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
