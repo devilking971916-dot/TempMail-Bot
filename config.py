fcd6d2ad407539333c7dcb4f7f44d5e015955949
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 01:37:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "34446649"  # Replace this API ID with your actual API ID
+API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8772312754:AAEken-freWxFhAHuVZ8iRsUT_oerjz3fT8"  # Replace this BOT_TOKEN
