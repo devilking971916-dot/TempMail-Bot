fa62c59e4ee83fe5d58076c99a28fe36e588578f
anujedits42-byte <anujedits42@gmail.com>
2026-04-08 17:13:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@
 # Pyrogram setup
 API_ID = "34446649"  # Replace this API ID with your actual API ID
 API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "8772312754:AAEken-freWxFhAHuVZ8iRsUT_oerjz3fT8"  # Replace this BOT_TOKEN
+BOT_TOKEN = "8191291880:AAEZJMVjGnXu01EEml7ACxLvsfVI2-0CROc"  # Replace this BOT_TOKEN
