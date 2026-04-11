cb3c5906391a51c29be6841af9da1c438920eb4a
devilking971916-dot <devilking971916@gmail.com>
2026-04-11 14:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Pyrogram setup
 API_ID = 34446649  # ✅ integer
 API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"
-BOT_TOKEN = "8747283809:AAG8fwnXeTt77duF_viYA6FbrmhzvWUo1U8"
+BOT_TOKEN = "8667684753:AAFbJbj4VWBZHvMlZ525elePDU-cdKasu7o"
