ad9776e3020365a0ea1b443a81a48b98928cd2fb
devilking971916-dot <devilking971916@gmail.com>
2026-04-19 21:48:25 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-from pyrogram.types import BotCommand
-
-async def set_bot_commands_list(app):
-    commands = [
-        BotCommand("start", "Start the bot and show welcome message"),
-        BotCommand("tmail", "Generate a random temporary email"),
-        BotCommand("cmail", "Check latest 10 emails"),
-    ]
-    await app.set_bot_commands(commands)
